8316568c99452c2f8b76d742ef151b55faae2a8a
mcestuya <93035929+mcestuya@users.noreply.github.com>
2022-04-03 20:41:19 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by{" "}
-        <a href="https://lucid-feynman-8ef81b.netlify.app/" target="_blank">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
           {" "}
-          Chessa Estuya{" "}
-        </a>
-        and is
-        <a
-          href="https://github.com/mcestuya/react-weather-search-app"
-          target="blank"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+          This project was coded by{" "}
+          <a href="https://lucid-feynman-8ef81b.netlify.app/" target="_blank">
+            {" "}
+            Chessa Estuya{" "}
+          </a>
+          and is
+          <a
+            href="https://github.com/mcestuya/react-weather-search-app"
+            target="blank"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
