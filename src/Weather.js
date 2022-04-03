e83598ab18d3cd731ed6a15e3b6a074999a847f9
mcestuya <93035929+mcestuya@users.noreply.github.com>
2022-04-03 22:12:51 +0100

    added html + button

---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Philippines</h1>
+      <ul>
+        <li> Wednesday 18:00 </li>
+        <li> Mostly Cloudy </li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 2%</li>
+            <li>Humidity: 78%</li>
+            <li>Wind: 6 mph</li>
+          </ul>
+        </div>
+      </div>
+      Hello from Weather
+    </div>
+  );
+}
