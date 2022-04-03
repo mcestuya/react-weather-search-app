8deb07e8b48612482ca2f8ce4b0e6c46a4f9e7ac
mcestuya <93035929+mcestuya@users.noreply.github.com>
2022-04-03 18:27:46 +0100

    added a footer and hyperlinks

--- src/App.js
+++ src/App.js
@@ -2,5 +2,25 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a href="https://lucid-feynman-8ef81b.netlify.app/" target="_blank">
+          {" "}
+          Chessa Estuya{" "}
+        </a>
+        and is
+        <a
+          href="https://github.com/mcestuya/react-weather-search-app"
+          target="blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
